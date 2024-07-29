780957b793232cc0b0eb25ac540f23b741d98eb9
Khuza1ma <khuzaimanrko@gmail.com>
2024-07-29 21:15:26 +0530

    Added sqflite dependency

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        title: 'Squbit', debugShowCheckedModeBanner: false, home: HomeView());
+      title: 'Squbit',
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
   }
 }
